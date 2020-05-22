6eee97835bc6e53ae4b541bea4a70d9d44db7e50
joshchen984 <joshchen984@gmail.com>
2020-05-22 16:50:07 +0000

    added button increasing in size when mouse touches it

--- Button.py
+++ Button.py
@@ -8,15 +8,21 @@ class Button:
     self.size = size
     self.rectColor = rectColor
     self.changeY = changeY
-    self.font = pygame.font.SysFont(None, self.size)
-    self.rendered_text = self.font.render(self.msg, True, self.color)
-    self.rect = self.rendered_text.get_rect()
+    # self.font = pygame.font.SysFont(None, self.size)
+    # self.rendered_text = self.font.render(self.msg, True, self.color)
+    # self.rect = self.rendered_text.get_rect()
+    self.createButton()
     self.clicking = False
 
   def display(self, win, winWidth, winHeight):
     self.rect.center = (winWidth//2, winHeight//2+self.changeY)
     pygame.draw.rect(win, self.rectColor, self.rect)
     win.blit(self.rendered_text, self.rect)
+
+  def createButton(self, changeSize = 0):
+    self.font = pygame.font.SysFont(None, self.size + changeSize)
+    self.rendered_text = self.font.render(self.msg, True, self.color)
+    self.rect = self.rendered_text.get_rect()
 
   def check_clicking(self, action, checked_clicking = False, isClicking = False):
     mouse_x, mouse_y = pygame.mouse.get_pos()
@@ -29,9 +35,11 @@ class Button:
           if(event.button == 1):
             self.clicking = True
 
-    if(self.rect.collidepoint((mouse_x, mouse_y)) and self.clicking):
-      action()
-      return (False, self.clicking)
+    if(self.rect.collidepoint((mouse_x, mouse_y))):
+      self.createButton(10)
+      if(self.clicking):
+        action()
+        return (False, self.clicking)
       
     return (True, self.clicking)
 
